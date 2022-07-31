f2b2270a6f0449028d039defb1067cb832785803
Ahmed-AbouElfetouh <ahmed_mohammed1992@outlook.com>
2022-08-01 00:48:29 +0200

    Dashboard Project Files

--- dist/js/script.js
+++ dist/js/script.js
@@ -3,12 +3,27 @@ const messageCategories = document.querySelectorAll(
   '.emails-box .sidebar-menu .messages-categories > div',
 );
 
+const allIcons = document.querySelector('#icons');
+
+function show_hide_icons() {
+  let checkBox = document.getElementById('select');
+  if (checkBox) {
+    checkBox.addEventListener('change', function () {
+      if (checkBox.checked) {
+        allIcons.classList.add('show-hide');
+      } else {
+        allIcons.classList.remove('show-hide');
+      }
+    });
+  }
+}
+show_hide_icons();
 if (messageCategories) {
   messageCategories.forEach((element) => {
-    element.addEventListener('click', function() {
+    element.addEventListener('click', function () {
       messageCategories.forEach((el) => {
         el.classList.remove('active');
-        this.classList.add('active')
+        this.classList.add('active');
       });
     });
   });
