fd6502a2b4f6a1698f1a442d3243ab4299a23b55
Ahmed-AbouElfetouh <ahmed_mohammed1992@outlook.com>
2022-07-31 22:19:10 +0200

    Dashboard Project Files

--- dist/js/script.js
+++ dist/js/script.js
@@ -77,9 +77,23 @@ function sliderHandler() {
   theBullets[start - 1].classList.add('active');
   addOrRemoveDisableClass();
 }
-// Select the Header Varialbles
+// Select Header Varialbles
 const notificationsIcon = document.getElementById('notifications-icon');
 const dropdownMenu = document.getElementById('dropdown-menu');
+const toggleRightBtn = document.getElementById('toggle-right');
+// Select Sidebar Varialbles
+const sidebar = document.getElementById('sidebar');
+const toggleLeftBtn = document.getElementById('toggle-left');
+
+// Toggle Sidebar to Right
+toggleRightBtn.addEventListener('click', () => {
+  sidebar.style.left = '0';
+});
+
+// Toggle Sidebar to Left
+toggleLeftBtn.addEventListener('click', () => {
+  sidebar.style.cssText = '@media (max-width: 768px) {left: -240px}';
+});
 
 // Show - Hide Notifications List
 notificationsIcon.addEventListener('click', () => {
